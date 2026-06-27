c8f1a71d484f6d5da9ca440b0b61362bed43e590
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2026-06-27 16:24:34 +0200

    Improved REST exception handling to have a unified JSON structure

--- role-service/src/main/java/ovh/eukon05/lokit/roleservice/exception/GlobalGrpcExceptionHandler.java
+++ role-service/src/main/java/ovh/eukon05/lokit/roleservice/exception/GlobalGrpcExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.grpc.server.advice.GrpcExceptionHandler;
 @GrpcAdvice
 public class GlobalGrpcExceptionHandler {
 
-    @GrpcExceptionHandler
+    @GrpcExceptionHandler(IllegalArgumentException.class)
     public Status handleIllegalArgument(IllegalArgumentException e) {
         return Status.INVALID_ARGUMENT.withDescription(e.getMessage());
     }
