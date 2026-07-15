75b7b3816b83d014ec7c25d74c9faa34b02b607d
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2026-07-15 14:58:16 +0200

    Added a gRPC endpoint for checking if user exists in identity-service

---
+++ room-service/src/main/java/ovh/eukon05/lokit/roomservice/exception/GlobalGrpcExceptionHandler.java
@@ -0,0 +1,15 @@
+package ovh.eukon05.lokit.roomservice.exception;
+
+import io.grpc.Status;
+import org.springframework.grpc.server.advice.GrpcAdvice;
+import org.springframework.grpc.server.advice.GrpcExceptionHandler;
+
+@GrpcAdvice
+public class GlobalGrpcExceptionHandler {
+
+    @GrpcExceptionHandler(IllegalArgumentException.class)
+    public Status handleIllegalArgument(IllegalArgumentException e) {
+        return Status.INVALID_ARGUMENT.withDescription(e.getMessage());
+    }
+
+}
